2ebdeaf6f9b2ee65132d3258603cdb3f5337531a
Daniel Coppi <daniel.coppi.dc@gmail.com>
2024-09-29 11:53:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2F4858",
+        secondary: "#3EB489",
+        secondaryDark: "#205642",
+      },
+    },
+  },
+  plugins: [],
+};
