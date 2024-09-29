859b5552f714a746c2ff58a012b4f2740ca712b5
Daniel <daniel.coppi.dc@gmail.com>
2024-09-29 20:32:02 +0200

    started about us section

--- src/app/page-home/carousel-home/carousel-home.component.ts
+++ src/app/page-home/carousel-home/carousel-home.component.ts
@@ -12,6 +12,7 @@ export class CarouselHomeComponent implements OnInit {
     'https://images.unsplash.com/photo-1604335398980-ededcadcc37d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1517677208171-0bc6725a3e60?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1545173168-9f1947eebb7f?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1604335398557-3f39109c7b4c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ];
 
   i: number = 0;
--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselHomeComponent } from './carousel-home/carousel-home.component';
+import { AboutSectionComponent } from './about-section/about-section.component';
 
 @Component({
   selector: 'app-page-home',
   standalone: true,
-  imports: [CarouselHomeComponent],
+  imports: [CarouselHomeComponent, AboutSectionComponent],
   templateUrl: './page-home.component.html',
   styleUrl: './page-home.component.css',
 })
